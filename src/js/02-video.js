277d47d786a49252421f735b8206b577d750ff4a
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-05-21 23:38:50 +0300

    finish task 1-2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const CURRENT_TIME = 'videoplayer-current-time';
+
+const player = new Player('vimeo-player');
+
+const getCurrentTime = ({ seconds }) => {
+  // console.log(seconds);
+  localStorage.setItem(CURRENT_TIME, seconds);
+};
+
+const savedCurrentTime = localStorage.getItem(CURRENT_TIME);
+
+player.setCurrentTime(savedCurrentTime);
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+// player.off('timeupdate', getCurrentTime);
