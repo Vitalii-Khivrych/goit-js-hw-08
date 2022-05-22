65725b39e7727585b2cdd307925f952cf200b417
Vitalii Khivrych <khivr.vitali@gmail.com>
2022-05-22 15:13:14 +0300

    finish all task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,53 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+const SAVE_DATA_KEY = 'feedback-form-state';
+
+const formData = {};
+
+populateFormFields();
+
+refs.form.addEventListener('input', throttle(onSaveFormField, 500));
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onSaveFormField(evt) {
+  formData[evt.target.name] = evt.target.value;
+  // console.log(formData);
+
+  localStorage.setItem(SAVE_DATA_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  // const {
+  //   elements: { email, message },
+  // } = evt.currentTarget;
+  // sentDataValues = { email: email.value, message: message.value };
+  // console.log(sentDataValues);
+
+  console.log(formData);
+
+  evt.currentTarget.reset();
+
+  localStorage.removeItem(SAVE_DATA_KEY);
+}
+
+function populateFormFields() {
+  const formStringValue = localStorage.getItem(SAVE_DATA_KEY);
+
+  if (formStringValue) {
+    const formObjectValue = JSON.parse(formStringValue);
+
+    for (const key in formObjectValue) {
+      formData[key] = formObjectValue[key];
+    }
+
+    refs.form.elements.email.value = formData.email ? formData['email'] : '';
+
+    refs.form.elements.message.value = formData.message ? formData['message'] : '';
+  }
+}
